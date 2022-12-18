ce7e9ea49709d9042fc9939317a326178324e06f
Fábio Ueno <fabio.t.ueno@gmail.com>
2022-12-18 17:08:52 -0300

    feat(log): Configure and add applcation logs

--- config/config.exs
+++ config/config.exs
@@ -3,3 +3,5 @@ import Config
 import_config "#{Mix.env()}.exs"
 
 config :issues, github_url: "https://api.github.com"
+
+config :logger, compile_time_purge_matching: [[lower_level_than: :info]]
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,7 +1,11 @@
 defmodule Issues.GithubIssues do
+  require Logger
+
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -12,6 +16,9 @@ defmodule Issues.GithubIssues do
   defp issues_url(user, project), do: "#{@github_url}/repos/#{user}/#{project}/issues"
 
   defp handle_response({_, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status_code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_for_error(),
       body |> Poison.Parser.parse!()
